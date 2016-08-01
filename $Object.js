63c4b57f9e0e46f4f99224f77ba52ebf2a5473bf
guiga-zalu <guiga_zalu@hotmail.com.br>
2016-08-01 20:34:46 -0300

    Create $Object.js

---
+++ $Object.js
@@ -0,0 +1,28 @@
+$Array = {
+	each : function(arr, call){
+		var ret = [];
+		for(var i = 0; i < arr.length; i++)
+			ret.push(call(arr[i]));
+		return ret;
+	},
+};
+$String = {
+	lessLast : function(string, str){
+		return string.slice(0, string.lastIndexOf(str));
+	},
+	searchAll : function(string, str, index){
+		index = index > 0 ? index : 0;
+		var ret = [], l = str.length;
+		while(string.search(str, index) > -1){
+			ret.push(string.search(str, index));
+			index += l;
+		}
+		return ret.length ? ret : 0;
+	},
+	indexes : function(string, arr){
+		var ret = [];
+		for(var i = 0; i < arr.length; i++)
+			ret.push(string[ arr[i] ]);
+		return ret.length ? ret : 0;
+	}
+};
