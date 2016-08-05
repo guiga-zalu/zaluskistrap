1c69e2c95b2bd8f8a684b4721c3e45efd7db4fd8
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2016-08-05 12:45:48 -0300

    Update $Constructor.js

--- $Constructor.js
+++ $Constructor.js
@@ -1,17 +1,19 @@
 $Constructor = {
-	init : function(callback){
-		for(var i = 0, j = this.functions.length; i < j; i++)
-			this[this.functions[i]]();
-		if(callback) return callback();
+	init : function(){
+		for(let i of this.functions)
+			(i[0] ? this[i]() : i());
 	},
+	functions : ['video'],
 	video : function(){
 		var ele = document.querySelectorAll('[yt-vid]');
 		if(!ele) return 0;
-		for(var i = 0, b, id, data; i < ele.length; i++){
+		var b, id, data;
+		for(let i in ele){
 			id = ele[i].getAttribute('yt-id');
 			data = ele[i].getAttribute('yt-data') || '&';
 			b = $Usefull.create('iframe', ele[i]);
 			b.src = 'https://www.youtube.com/watch?v=' + id + data;
+			b = null;
 		}
 	}
 };
