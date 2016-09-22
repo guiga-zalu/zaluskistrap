d1f28f2cd832d47a41f4fd468cf0a3788b7f6327
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2016-09-22 20:54:54 -0300

    Added init() reference creator for sub-objects

--- $Canvas.js
+++ $Canvas.js
@@ -1,33 +1,37 @@
 $Canvas = {
-	that : this,
 	create : function(element, mode){
 		if(element && parent){
-			that.element = element;
-			return that.element;
+			this.element = element;
+			return this.element;
 		}
 		element = element || document.body;
-		that.element = document.createElement('canvas');
-		element.appendChild(that.element);
-		return that.element;
+		this.element = document.createElement('canvas');
+		element.appendChild(this.element);
+		return this.element;
 	},
 	contextSet : function(type){
 		type = type || '2d';
-		that.context = that.element.getContext(type);
-		return that.context;
+		this.context = this.element.getContext(type);
+		return this.context;
 	},
 	imgData : {
 		imgDatas : [],
 		save : function(x, y, cx, cy){
-			this.imgDatas.push(that.context.createImageData(x, y, cx, cy));
+			this.imgDatas.push(this.parent.context.createImageData(x, y, cx, cy));
 		},
 		use : function(index, x, y, cx, cy){
-			that.context.drawImage(this.imgDatas[index], x, y, cx, cy);
+			this.parent.context.drawImage(this.imgDatas[index], x, y, cx, cy);
 		},
 		toDataURI : function(index){
 			return this.imgDatas[index].toDataURL();
 		}
 	}
-}
+	init : function() {
+		//By : Mik, http://stackoverflow.com/questions/2980763/javascript-objects-get-parent#answer-10170826
+		for(var x in arguments) this[arguments[x]].parent = this;
+		return this;
+	}
+}.init('imgData');
 $Canvas.new = function(){
 	return Object.create($Canvas);
 }
