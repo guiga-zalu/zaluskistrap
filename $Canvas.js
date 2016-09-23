2a5e660195507a2d216e3c2dec8450d7400e1ad3
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2016-09-23 12:25:05 -0300

    Update $Canvas.js

--- $Canvas.js
+++ $Canvas.js
@@ -25,7 +25,7 @@ $Canvas = {
 		toDataURI : function(index){
 			return this.imgDatas[index].toDataURL();
 		}
-	}
+	},
 	init : function() {
 		//By : Mik, http://stackoverflow.com/questions/2980763/javascript-objects-get-parent#answer-10170826
 		for(var x in arguments) this[arguments[x]].parent = this;
