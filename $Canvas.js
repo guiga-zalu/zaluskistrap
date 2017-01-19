32fe55319bf47fc563eef06a12fa111b0c43ec51
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2017-01-19 13:06:23 -0300

    Update $Canvas.js

--- $Canvas.js
+++ $Canvas.js
@@ -1,37 +1,55 @@
-$Canvas = {
-	create : function(element, mode){
-		if(element && parent){
-			this.element = element;
-			return this.element;
-		}
-		element = element || document.body;
-		this.element = document.createElement('canvas');
-		element.appendChild(this.element);
-		return this.element;
-	},
-	contextSet : function(type){
-		type = type || '2d';
-		this.context = this.element.getContext(type);
-		return this.context;
-	},
-	imgData : {
-		imgDatas : [],
-		save : function(x, y, cx, cy){
-			this.imgDatas.push(this.parent.context.createImageData(x, y, cx, cy));
-		},
-		use : function(index, x, y, cx, cy){
-			this.parent.context.drawImage(this.imgDatas[index], x, y, cx, cy);
-		},
-		toDataURI : function(index){
-			return this.imgDatas[index].toDataURL();
-		}
-	},
-	init : function() {
-		//By : Mik, http://stackoverflow.com/questions/2980763/javascript-objects-get-parent#answer-10170826
-		for(var x in arguments) this[arguments[x]].parent = this;
-		return this;
+function Canvas(ele, context, width, height){
+	var isCanvas = ele && ele.tagName.toLowerCase() == 'canvas';
+	if(!ele || !isCanvas){
+		var _ele = ele || document.body;
+		ele = document.createElement('canvas');
+		_ele.appendChild(ele);
 	}
-}.init('imgData');
-$Canvas.new = function(){
-	return Object.create($Canvas);
+	context = context || '2d';
+	this.context = ele.getContext(context);
+	if(width){
+		this.width = ele.width = width;
+		if(height) this.height = ele.height = height;
+		else this.height = ele.height = width;
+	}
+	this.multiApply = function(method){
+		var haveMethod = (method in this);
+		if(!haveMethod) method = 'context';
+		var i = haveMethod ? 1 : 0,
+		arg = arguments;
+		for(var func; i < arg.length; i++){
+			//this.'object'.'function'()
+			if((typeof arg[i]).toLowerCase()){
+				//this.'context'.'beginPath'()
+				this[ method ][ arg[i] ]();
+			}else{
+				//this.'context'.'moveTo'(x, y)
+				func = arg[i].shift();
+				this[ method ][ func ].apply(this[ method ], arg[i]);
+			}
+		}
+	};
+/*
+Use:
+x.multiApply('context', ['beginPath'], ['moveTo', 0, 0], ['lineTo', 100, 0], ['lineTo', 100, 100], ['closePath']);
+x.multiApply('context', 'beginPath', ['moveTo', 0, 0], ['lineTo', 100, 0], ['lineTo', 100, 100], 'closePath');
+x.multiApply(['beginPath'], ['moveTo', 0, 0], ['lineTo', 100, 0], ['lineTo', 100, 100], ['closePath']);
+x.multiApply('beginPath', ['moveTo', 0, 0], ['lineTo', 100, 0], ['lineTo', 100, 100], 'closePath');
+*/
+	this.imgData = {
+		saves : [],
+		save : (x, y, cx, cy) => this.saves.push(
+			this.parent.context.createImageData(x, y, cx, cy)
+		),
+		use : (index, x, y, cx, cy) => this.parent.context.drawImage(
+			this.saves[index], x, y, cx, cy
+		),
+		toDataURI : (index) => this.saves[index].toDataURL()
+	};
+	this.init = function(){
+//By: Mik,
+//http://stackoverflow.com/questions/2980763/javascript-objects-get-parent#answer-10170826
+		for(var x in arguments) this[arguments[x]].parent = this;
+	};
+	this.init('imgData');
 }
