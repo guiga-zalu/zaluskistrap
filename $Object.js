38a3ee1447ff7000fdec469f8dbe7ad90cde13bb
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2017-01-26 17:29:37 -0300

    Update $Object.js

--- $Object.js
+++ $Object.js
@@ -24,5 +24,35 @@ $String = {
 		for(let i in arr)
 			ret.push(string[ arr[i] ]);
 		return ret.length ? ret : 0;
+	},
+	split : function(string, arg){
+		var ret = [];
+		if(typeof arg === 'number'){
+			var l = string.length;
+			arg = arg < l ? arg : arg % l;
+			var j = Math.floor(l/arg);
+			for(var i = 0; i < l; i += j) ret.push(string.slice(i, j));
+		}else{
+			var args = arguments.shift();
+			/*
+			i : itinerador argumento
+			j : itinerador [k]
+			k : [ret] passado
+			l : itinerador [m]
+			m : [k] dividido
+			*/
+			for(var i = 0, j, k, l, m; i < args.length; i++){
+				if(ret.length){
+					k = ret;
+					ret = [];
+					for(j = 0; j < k.length; j++){
+						for(m = k[j].split(args[i]), l = 0; l < m; l++) ret.push(m[l]);
+					}
+				}else{
+					ret = string.split( arg );
+				}
+			}
+		}
+		return ret;
 	}
 };
