2e996b15e8d0c8f80e11122c78214e2eaaf2ca80
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2020-11-17 18:20:46 -0300

    Update $Usefull.js
    randomID não. uuID.

--- $Usefull.js
+++ $Usefull.js
@@ -1,18 +1,18 @@
-$Usefull = {
-	request : function(url, method, async, mode, callback){
+class $Usefull{
+	static request(url, method, async, mode, callback){
 		method = method || 'GET';
 		async = (async == undefined) ? true : async;
 		var sec = new XMLHttpRequest();
 		sec.onreadystatechange = function(){
 			if(sec.readyState == 4 && sec.status == 200){
-				var response = (mode) ? sec.responseText : sec.responseXML;
+				var response = mode ? sec.responseText : sec.responseXML;
 				callback(response);
 			}
 		}
 		sec.open(method, url, async);
 		sec.send();
-	},
-	spliter : function(url, spacer, ele, callback){
+	}
+	static spliter(url, spacer, ele, callback){
 		function split(txt){
 			ele.innerText = txt;
 			var a = true;
@@ -24,8 +24,8 @@ $Usefull = {
 			ele.innerHTML = text;
 		}
 		this.request(url, 'GET', true, 1, split);
-	},
-	test : function(obj, type){
+	}
+	static test(obj, type){
 		if(typeof type !== 'string'){
 			if(obj istanceof type
 			|| obj === type) return true;
@@ -35,17 +35,17 @@ $Usefull = {
 			obj === type
 			|| obj.constructor.toString().toLowerCase().includes(type.toLowerCase())
 		);
-	},
-	exists : function(arg, _default){
+	}
+	static exists(arg, _default){
 		return arg === undefined ? _default : arg;
 	}
-};
+}
 $Usefull.defaultValue = $Usefull.exists;
 if($Math){
 //	if('randomChoice' in $Math){
-		$Usefull.randomID = (length = 8, lowerCase = false) => {
+		$Usefull.uuID = function uuID(length = 8, lowerCase = false){
 			var i;
-			if(!('chars' in $Usefull.randomID)){
+			if(!('chars' in $Usefull.uuID)){
 				var arr = [];
 				//adding only lower case letters
 				//for(i = ; i < ; i++) arr.push(String.fromCharCode(i));
@@ -54,27 +54,29 @@ if($Math){
 				//for(i = ; i < ; i++) arr.push(String.fromCharCode(i));
 				arr.push('$');
 				arr.push('_')
-				$Usefull.randomID.chars = arr;
+				$Usefull.uuID.chars = arr;
 			}
-			if(!('usedIDs' in $Usefull.randomID)) $Usefull.randomID.usedIDs = {};
+			if(!('usedIDs' in $Usefull.uuID)) $Usefull.uuID.usedIDs = {};
 			var ret, joined;
 			do{
 				ret = [];
 				for(i = 0; i < length; i++){
-					ret[i] = $Math.randomChoice.apply(null, $Usefull.randomID.chars);
+					ret[i] = $Math.randomChoice.apply(null, $Usefull.uuID.chars);
 					ret[i] = ret[i]['to' + $Math.randomChoice('Lower', 'Upper') + 'Case']();
 				}
 				joined = ret.join('');
 				if(lowerCase) joined = joined.toLowerCase();
-			}while(joined in $Usefull.randomID.ids);
-			$Usefull.randomID.usedIDs[ joined ] = true;
+			}while(joined in $Usefull.uuID.ids);
+			$Usefull.uuID.usedIDs[ joined ] = true;
 			return joined;
 		}
 //	}
 }
 
-ImageData.prototype.constructor.coords = (x, y) => (this.width * y + x) << 2;
-ImageData.prototype.constructor.remove = function remove(r, g, b){
+ImageData.prototype.coords = function coords(x, y){
+	return (this.width * y + x) << 2;
+};
+ImageData.prototype.remove = function remove(r, g, b){
 	for(var j = this.data.length, i = 0; i < j; i += 4){
 		if(this.data[i] == r && this.data[i + 1] == g && this.data[i + 2] == b) this.data[i + 4] = 0;
 	}
